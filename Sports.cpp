b157704110bd0ed99e30404e02d704b01515380c
Agah Özdemir <72024578+aagahoz@users.noreply.github.com>
2021-05-28 23:37:20 +0300

    Update Sports.cpp

--- Sports.cpp
+++ Sports.cpp
@@ -33,29 +33,104 @@ ostream& operator << (ostream &out, const Basketball &obj)
 	return out;
 }
 		
-void Basketball::setCalForMin(int tempCal)
-{
-		this->calForMin = tempCal;
-}
-
-int Basketball::getCalForMin()
-{
-		return this->calForMin;
-}
-
-void Basketball::setTotalCal(int tempCal)
-{
-		this->totalCalForExercise = tempCal;
-}
-		
-int Basketball::getTotalCal()
-{
-		return this->totalCalForExercise;
-}
+void Basketball::setCalForMin(int tempCal)							{		this->calForMin = tempCal;				}
+int Basketball::getCalForMin()										{		return this->calForMin;					}
+void Basketball::setTotalCal(int tempCal)							{		this->totalCalForExercise = tempCal;	}		
+int Basketball::getTotalCal()										{		return this->totalCalForExercise;		}
 
 int Basketball::amountCalForExercise(int min)
 {
 		totalCalForExercise = min*this->calForMin;
 		return totalCalForExercise;
 }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+Football::Football()
+{
+	this->calForMin = 6;
+	this->totalCalForExercise = 0;
+}
+Football::Football(int calForMin, int totalCalForExercise)
+{
+		this->calForMin = calForMin;
+		this->totalCalForExercise = totalCalForExercise;
+}
+Football::Football(const Football &newObje)
+{
+	this->calForMin = newObje.calForMin;
+	this->totalCalForExercise = newObje.totalCalForExercise;
+}
+Football& Football::operator = (const Football &newObje)
+{
+		this->calForMin = newObje.calForMin;
+		this->totalCalForExercise = newObje.totalCalForExercise;
+		return *this;
+}
+		
+ostream& operator << (ostream &out, const Football &obj)
+{
+	out << "Birim Calori > ";
+	out << obj.calForMin << endl;
+	out << "Toplam Calori > ";
+	out << obj.totalCalForExercise << endl << endl;
+	return out;
+}
+		
+void Football::setCalForMin(int tempCal)						{		this->calForMin = tempCal;				}
+int Football::getCalForMin()									{		return this->calForMin;					}
+void Football::setTotalCal(int tempCal)							{		this->totalCalForExercise = tempCal;	}
+int Football::getTotalCal()										{		return this->totalCalForExercise;		}
+
+int Football::amountCalForExercise(int min)
+{
+		totalCalForExercise = min*this->calForMin;
+		return totalCalForExercise;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+Tennis::Tennis()
+{
+	this->calForMin = 6;
+	this->totalCalForExercise = 0;
+}
+Tennis::Tennis(int calForMin, int totalCalForExercise)
+{
+		this->calForMin = calForMin;
+		this->totalCalForExercise = totalCalForExercise;
+}
+Tennis::Tennis(const Tennis &newObje)
+{
+	this->calForMin = newObje.calForMin;
+	this->totalCalForExercise = newObje.totalCalForExercise;
+}
+Tennis& Tennis::operator = (const Tennis &newObje)
+{
+		this->calForMin = newObje.calForMin;
+		this->totalCalForExercise = newObje.totalCalForExercise;
+		return *this;
+}
+		
+ostream& operator << (ostream &out, const Tennis &obj)
+{
+	out << "Birim Calori > ";
+	out << obj.calForMin << endl;
+	out << "Toplam Calori > ";
+	out << obj.totalCalForExercise << endl << endl;
+	return out;
+}
+		
+void Tennis::setCalForMin(int tempCal)			{		this->calForMin = tempCal;				}
+int Tennis::getCalForMin()						{		return this->calForMin;					}
+void Tennis::setTotalCal(int tempCal)			{		this->totalCalForExercise = tempCal;	}		
+int Tennis::getTotalCal()						{		return this->totalCalForExercise;		}
+
+int Tennis::amountCalForExercise(int min)
+{
+		totalCalForExercise = min*this->calForMin;
+		return totalCalForExercise;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
 
