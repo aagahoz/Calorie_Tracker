52b36f2cc89479de2990ed349bb492e6610d3378
Agah Özdemir <72024578+aagahoz@users.noreply.github.com>
2021-05-28 23:38:13 +0300

    Update User.h

--- User.h
+++ User.h
@@ -1,40 +1,51 @@
 #ifndef libraryUser_h_
 #define libraryUser_h_
-
+#include "Sports.h"
+#include "Meals.h"
 #include <iostream>
 using namespace std;
-
 
 class User
 {
 		int ID;
 		string Name;
 		string Surname;
+		int age;
+		int weight;
 		int week;
+		int totalCalori;
+
 	public:
 		User();
-		User(int ID, string Name, string Surname, int Week);
+		User(int ID, string Name, string Surname, int age, int weight, int Week, int totalCalori);
 		User(const User &obj);
 		User& operator = (const User &obj);
 		
 		friend ostream& operator << (ostream &out, const User &obj);
-		User operator + (User const &obj) {	return User(ID + obj.ID, "Null", "Null", week + obj.week);}
-
+		User operator + (User const &obj);
 
 		int getID();
 		string getName();
 		string getSurname();
+		int getAge();
+		int getWeight();
 		int getWeek();
+		int getTotalCalori();
 		void setID(int ID);
 		void setName(int Name);
 		void setSurname(int Surname);
+		void setAge(int age);
+		void setWeight(int weight);
 		void setWeek(int Week);
+		void setTotalCalori(int totalCalori);
+
+		void sportCalori(int sportMin, Basketball obj);
+		void sportCalori(int sportMin, Football obj);
+		void sportCalori(int sportMin, Tennis obj);
+
+		void mealCalori(Breakfast obj, int portion);
+		void mealCalori(Lunch obj, int portion);
+		void mealCalori(Dinner obj, int portion);
 
 };
-
-
-
-
-
-
 #endif
