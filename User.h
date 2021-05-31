7e916caee506414aa2941e6312180ad6cfa36652
Agah Özdemir <72024578+aagahoz@users.noreply.github.com>
2021-05-31 11:33:48 +0300

    Update User.h

--- User.h
+++ User.h
@@ -5,6 +5,18 @@
 #include <iostream>
 using namespace std;
 
+struct Counts
+		{
+			int countBasketball = 0;
+			int countFootball = 0;
+			int countTennis = 0;
+			int countSwimming = 0;
+			int countBreakfast = 0;
+			int countLunch = 0;
+			int countDinner = 0;
+		};
+typedef struct Counts counts;
+
 class User
 {
 		int ID;
@@ -13,14 +25,16 @@ class User
 		int age;
 		int weight;
 		int week;
-		int totalCalori;
+		int burnedCalory;
+		int takenCalory;
+		counts count;
 
 	public:
 		User();
-		User(int ID, string Name, string Surname, int age, int weight, int Week, int totalCalori);
+		User(int ID, string Name, string Surname, int age, int weight, int Week, int burnedCalory, int takenCalory);
 		User(const User &obj);
 		User& operator = (const User &obj);
-		
+
 		friend ostream& operator << (ostream &out, const User &obj);
 		User operator + (User const &obj);
 
@@ -30,22 +44,24 @@ class User
 		int getAge();
 		int getWeight();
 		int getWeek();
-		int getTotalCalori();
+		int getBurnedCalory();
+		int getTakenCalory();
 		void setID(int ID);
 		void setName(int Name);
 		void setSurname(int Surname);
 		void setAge(int age);
 		void setWeight(int weight);
 		void setWeek(int Week);
-		void setTotalCalori(int totalCalori);
+		void setBurnedCalory(int burnedCalory);
+		void setTakenCalory(int takenCalory);
+		
+		void enterInfo();
 
-		void sportCalori(int sportMin, Basketball obj);
-		void sportCalori(int sportMin, Football obj);
-		void sportCalori(int sportMin, Tennis obj);
-
-		void mealCalori(Breakfast obj, int portion);
-		void mealCalori(Lunch obj, int portion);
-		void mealCalori(Dinner obj, int portion);
-
+		void sportCalori(int sportMin, int sportType);
+		void mealCalori(int mealType, int portion);
+		
+		void mealInfoWeekly();
+		void sportInfoWeekly();
+		void totalCalorisInfoWeekly();
 };
 #endif
