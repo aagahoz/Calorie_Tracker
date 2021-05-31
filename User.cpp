37379f5e192103442808f62cbaca82d8de968dc8
Agah Özdemir <72024578+aagahoz@users.noreply.github.com>
2021-05-31 11:33:35 +0300

    Update User.cpp

--- User.cpp
+++ User.cpp
@@ -4,13 +4,16 @@ using namespace std;
 
 User::User()
 {
-	ID = -1;
+	ID = 0;
 	Name = "Null";
 	Surname = "Null";
-	week = -1;
-	totalCalori = 0;
+	age = 0;
+	weight = 0;
+	week = 0;
+	takenCalory = 0;
+	burnedCalory = 0;
 }
-User::User(int ID, string Name, string Surname, int age, int weight, int Week, int totalCalori)
+User::User(int ID, string Name, string Surname, int age, int weight, int week, int burnedCalory, int takenCalory)
 {
 	this->ID = ID;
 	this->Name = Name;
@@ -18,7 +21,8 @@ User::User(int ID, string Name, string Surname, int age, int weight, int Week, i
 	this->age = age;
 	this->weight = weight;
 	this->week = week;
-	this->totalCalori = totalCalori;
+	this->burnedCalory = burnedCalory;
+	this->takenCalory = takenCalory;
 }
 User::User(const User &obj)
 {
@@ -28,7 +32,8 @@ User::User(const User &obj)
 	this->age = obj.age;
 	this->weight = obj.weight;
 	this->week = obj.week;
-	this->totalCalori = obj.totalCalori;
+	this->burnedCalory = obj.burnedCalory;
+	this->takenCalory = obj.takenCalory;
 	
 }
 User& User::operator = (const User &obj)
@@ -39,22 +44,24 @@ User& User::operator = (const User &obj)
 	this->age = obj.age;
 	this->weight = obj.weight;
 	this->week = obj.week;
-	this->totalCalori = obj.totalCalori;
+	this->burnedCalory = obj.burnedCalory;
+	this->takenCalory = obj.takenCalory;
 	return *this;
 }
 User User::operator + (const User &obj)
 {
-	return User(0, "Null", "Null", 0, 0, 0, this->totalCalori + obj.totalCalori);
+	return User(0, "Null", "Null", 0, 0, 0, this->burnedCalory + obj.burnedCalory, this->takenCalory + obj.takenCalory);
 }
 ostream& operator << (ostream &out, const User &obj)
 {
-	out << "ID >		" << obj.ID << endl;
-	out << "Name >		" << obj.Name << endl;
-	out << "Surname >	" << obj.Surname << endl;
-	out << "Age >		" << obj.age << endl;
-	out << "Weight >	" << obj.weight << endl;
-	out << "Week >		" << obj.week << endl;
-	out << "Calori >	" << obj.totalCalori << endl << endl;
+	out << "ID >			" << obj.ID << endl;
+	out << "Name >			" << obj.Name << endl;
+	out << "Surname >		" << obj.Surname << endl;
+	out << "Age >			" << obj.age << endl;
+	out << "Weight >		" << obj.weight << endl;
+	out << "Week >			" << obj.week << endl;
+	out << "Burned Calori >		" << obj.burnedCalory << endl;
+	out << "Taken Calori >		" << obj.takenCalory << endl << endl;
 	return out;
 }
 
@@ -63,49 +70,111 @@ string User::getName()						{	return this->Name;				}
 string User::getSurname()					{	return this->Surname;			}
 int User::getWeek()							{	return this->week;				}
 int User::getAge()							{	return this->age;				}
-int User::getWeight()						{	return this->weight;				}
-int User::getTotalCalori()					{	return this->totalCalori;		}
+int User::getWeight()						{	return this->weight;			}
+int User::getBurnedCalory()					{	return this->burnedCalory;		}
+int User::getTakenCalory()					{	return this->takenCalory;		}
 
-void User::setID(int ID)					{	this->ID = ID;					}
-void User::setName(int Name)				{	this->Name = Name;				}
-void User::setSurname(int Surname)			{	this->Surname = Surname;		}
-void User::setAge(int Week)					{	this->week = week;				}
-void User::setWeight(int Week)				{	this->week = age;				}
+void User::setID(int ID)					{	this->ID = ID;						}
+void User::setName(int Name)				{	this->Name = Name;					}
+void User::setSurname(int Surname)			{	this->Surname = Surname;			}
+void User::setAge(int Week)					{	this->week = week;					}
+void User::setWeight(int Week)				{	this->week = age;					}
 void User::setWeek(int Week)				{	this->week = weight;				}
-void User::setTotalCalori(int totalCalori)	{	this->totalCalori = totalCalori;}
+void User::setBurnedCalory(int burnedCalory){	this->burnedCalory = burnedCalory;	}
+void User::setTakenCalory(int takenCalory)	{	this->takenCalory = takenCalory;	}
 
 //////////////////////////////////////////////////////////////////////////////////////////
 
-void User::sportCalori(int sportMin, Basketball obj)
+void User::enterInfo()
 {
-	obj.amountCalForExercise(sportMin);
-	this->totalCalori += obj.getTotalCal();
+	cout << "Enter new User Info!" << endl;
+	cout << "Name >	" ; 		cin >> this->Name;
+	cout << "Surname > " ; 		cin >> this->Surname;
+	cout << "Age > " ; 			cin >> this->age;
+	cout << "Weight > " ; 		cin >> this->weight;
+	cout << "Week > " ; 		cin >> this->week;
+	cout << "Taken Calory > " ;	cin >> this->burnedCalory;
+	cout << "Burned Calory > " ;cin >> this->takenCalory;
+
 }
-void User::sportCalori(int sportMin, Football obj)
+
+void User::sportCalori(int sportMin, int sportType)
 {
-	obj.amountCalForExercise(sportMin);
-	this->totalCalori += obj.getTotalCal();
+	if(sportType == 1)	
+	{
+		Basketball obj;	
+		obj.amountCalForExercise(sportMin);
+		this->burnedCalory += obj.getTotalCal();
+		this->count.countBasketball++; 
+	}
+	
+	if(sportType == 2)	
+	{
+		Football obj;
+		obj.amountCalForExercise(sportMin);
+		this->burnedCalory += obj.getTotalCal();
+		this->count.countFootball++;
+	}
+	if(sportType == 3)	
+	{
+		Tennis obj;
+		obj.amountCalForExercise(sportMin);
+		this->burnedCalory += obj.getTotalCal();
+		this->count.countTennis++;
+	}
+	if(sportType == 4)	
+	{
+		Swimming obj;
+		obj.amountCalForExercise(sportMin);
+		this->burnedCalory += obj.getTotalCal();
+		this->count.countSwimming++;
+	}
 }
-void User::sportCalori(int sportMin, Tennis obj)
-{
-	obj.amountCalForExercise(sportMin);
-	this->totalCalori += obj.getTotalCal();
-}
+
 
 ////////////////////////////////////////////////////////////////////////////////////////////
 
-void User::mealCalori(Breakfast obj, int portion)
+void User::mealCalori(int mealType, int portion)
 {
-	obj.amountCalForMeal(portion);
-	this->totalCalori -= obj.getCalTaken();
+	if(mealType == 1)
+	{
+		Breakfast obj;
+		obj.amountCalForMeal(portion);
+		this->takenCalory += obj.getCalTaken();
+		this->count.countBreakfast++;
+	}
+	if(mealType == 2)
+	{
+		Lunch obj;
+		obj.amountCalForMeal(portion);
+		this->takenCalory += obj.getCalTaken();
+		this->count.countLunch++;
+	}
+	if(mealType == 3)
+	{
+		Dinner obj;
+		obj.amountCalForMeal(portion);
+		this->takenCalory += obj.getCalTaken();
+		this->count.countDinner++;
+	}
 }
-void User::mealCalori(Lunch obj, int portion)
+
+void User::mealInfoWeekly()
 {
-	obj.amountCalForMeal(portion);
-	this->totalCalori -= obj.getCalTaken();
+	cout << "Breakfast	-->  " << this->count.countBreakfast << endl;
+	cout << "Lunch		-->  " << this->count.countLunch << endl;
+	cout << "Dinner		-->  " << this->count.countDinner << endl;
 }
-void User::mealCalori(Dinner obj, int portion)
+void User::sportInfoWeekly()
 {
-	obj.amountCalForMeal(portion);
-	this->totalCalori -= obj.getCalTaken();
+	cout << "Basketball	-->  " << this->count.countBasketball << endl;
+	cout << "Football	-->  " << this->count.countFootball << endl;
+	cout << "Tennis		-->  " << this->count.countTennis << endl;
+	cout << "Swimming	-->  " << this->count.countSwimming << endl;
+
+}
+void User::totalCalorisInfoWeekly()
+{
+	cout << "Consumed Calory	-->  " << this->takenCalory << endl;
+	cout << "Burned Calory	-->  " << this->burnedCalory << endl;
 }
